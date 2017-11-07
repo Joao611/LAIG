c6244463d265cb224801136fbccd84a04c4dcbee
Nuno Pereira <up201506265@fe.up.pt>
2017-11-07 13:03:59 +0000

    CircularAnimantion added

--- CircularAnimation.js
+++ CircularAnimation.js
@@ -2,9 +2,31 @@
  * CircularAnimation
  * @constructor
  */
-function CircularAnimation() {
-	
+var degToRad = Math.PI / 180;
+
+function CircularAnimation(speed, centerX, centerY, centerZ, radius, startAng, rotAng) {
+
+this.speed=speed;
+this.centerX=centerX;
+this.centerY=centerY;
+this.centerZ=centerZ;	
+this.radius=radius;
+this.startAng=startAng * degToRad;
+this.rotAng=rotAng * degToRad;
 };
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
 CircularAnimation.prototype.constructor = CircularAnimation;
+
+CircularAnimation.prototype.getTransform = function(t) {
+
+
+this.scene.pushMatrix();
+	this.scene.loadIdentity(); 
+		this.scene.translate(centerX,centerY,centerZ);
+			this.scene.rotate(startAng+(rotAng*t));
+			this.scene.translate(radius, 0, 0);
+		this.scene.translate(-centerX,-centerY,-centerZ);
+	this.transformMatrix = this.scene.getMatrix();
+this.scene.popMatrix();
+}
