4896d46d3fc0a1812de9ce802c2b4301ebe72e4e
Nuno Pereira <up201506265@fe.up.pt>
2017-11-14 13:14:13 +0000

    Circular animation now working properly

--- CircularAnimation.js
+++ CircularAnimation.js
@@ -12,7 +12,7 @@ class CircularAnimation extends Animation {
 		this.centerX=centerX;
 		this.centerY=centerY;
 		this.centerZ=centerZ;
-		this.radiu5s=radius;
+		this.radius=radius;
 		this.startAng=startAng * degToRad;
 		this.rotAng=rotAng * degToRad;
 	}
@@ -22,9 +22,10 @@ CircularAnimation.prototype.getTransform = function(t) {
 this.scene.pushMatrix();
 	this.scene.loadIdentity();
 		this.scene.translate(this.centerX,this.centerY,this.centerZ);
-			this.scene.rotate((this.startAng+(this.rotAng*t)),0,1,0);
+			this.scene.rotate(-(this.startAng+(this.rotAng*t)),0,1,0);
 			this.scene.translate(this.radius, 0, 0);
 		this.scene.translate(-this.centerX,-this.centerY,-this.centerZ);
 	this.transformMatrix = this.scene.getMatrix();
 this.scene.popMatrix();
+return this.transformMatrix;
 }
