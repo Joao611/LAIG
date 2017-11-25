69ba76f75466dd7812c0a7680fc9ec3c1a390edc
Nuno Pereira <up201506265@fe.up.pt>
2017-11-25 19:58:39 +0000

    Combo added, not complete

--- ComboAnimation.js
+++ ComboAnimation.js
@@ -1,5 +1,16 @@
 class ComboAnimation extends Animation {
-    constructor(scene) {
+    constructor(scene, animations) {
         super(scene);
+        animations = animations;
+        elapsedTime = 0;
     }
+    
+    getTransform(t){
+    for(let i=0; i< this.animations.lenght; i++){
+        elapsedTime +=  this.animations[i].totalTime;
+    while(t < elapsedTime){
+        this.animations[i].getTransform(t);
+    }
+    }
+}
 }
