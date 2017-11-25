814d400f33cef233f16ed776dd88f5aa3204af8f
Nuno Pereira <up201506265@fe.up.pt>
2017-11-25 22:34:58 +0000

    Some corrections

--- ComboAnimation.js
+++ ComboAnimation.js
@@ -1,16 +1,16 @@
 class ComboAnimation extends Animation {
     constructor(scene, animations) {
         super(scene);
-        animations = animations;
-        elapsedTime = 0;
+        this.animations = animations;
+        this.elapsedTime = 0;
     }
     
     getTransform(t){
-    for(let i=0; i< this.animations.lenght; i++){
-        elapsedTime +=  this.animations[i].totalTime;
-    while(t < elapsedTime){
-        this.animations[i].getTransform(t);
+        for(let i=0; i< this.animations.lenght; i++){
+            this.elapsedTime +=  this.animations[i].totalTime;
+            while(t < elapsedTime){
+                this.animations[i].getTransform(t);
+            }  
+        }
     }
-    }
-}
 }
