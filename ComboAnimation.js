f80206decff9213202805e2f3a5fbeecd8e60228
Nuno Pereira <up201506265@fe.up.pt>
2017-11-26 16:13:42 +0000

    still not working

--- ComboAnimation.js
+++ ComboAnimation.js
@@ -3,17 +3,18 @@ class ComboAnimation extends Animation {
         super(scene);
         this.animations = animations;
         this.elapsedTime = 0;
+        this.totalTime=0;
         for(let i=0; i< this.animations.lenght; i++){
             this.totalTime +=  this.animations[i].totalTime
         }
     }
-    
+
     getTransform(t){
         for(let i=0; i< this.animations.lenght; i++){
             this.elapsedTime +=  this.animations[i].totalTime;
             while(t < elapsedTime){
                 this.animations[i].getTransform(t);
-            }  
+            }
         }
     }
 }
