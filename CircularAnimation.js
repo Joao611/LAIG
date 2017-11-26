7ee286ea0c26f59d78e3f4ad5a63a676e6f03ed7
joao611 <joaofvde@gmail.com>
2017-11-26 14:42:33 +0000

    Fixed CircAnim

--- CircularAnimation.js
+++ CircularAnimation.js
@@ -29,7 +29,7 @@ class CircularAnimation extends Animation {
 			this.scene.translate(this.centerX, this.centerY, this.centerZ);
 			this.scene.rotate(-(this.startAng + (this.rotAng * t)), 0, 1, 0);
 			this.scene.translate(this.radius, 0, 0);
-			this.scene.translate(-this.centerX, -this.centerY, -this.centerZ);
+			this.scene.rotate(-Math.PI/2, 0, 1, 0);
 			this.transformMatrix = this.scene.getMatrix();
 		this.scene.popMatrix();
 
