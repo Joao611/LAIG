306c781938e08f488a9f9232b86543f1d01bf414
joao611 <joaofvde@gmail.com>
2017-11-26 14:55:56 +0000

    Fixed CircAnim rotation direction and made it more efficient - no more pushing and popping matrixes

--- CircularAnimation.js
+++ CircularAnimation.js
@@ -8,9 +8,7 @@ class CircularAnimation extends Animation {
 	constructor(scene, speed, centerX, centerY, centerZ, radius, startAng, rotAng) {
 		super(scene);
 		this.speed = speed;
-		this.centerX = centerX;
-		this.centerY = centerY;
-		this.centerZ = centerZ;
+		this.center = [centerX, centerY, centerZ];
 		this.radius = radius;
 		this.startAng = startAng * degToRad;
 		this.rotAng = rotAng * degToRad;
@@ -24,19 +22,29 @@ class CircularAnimation extends Animation {
 			t = 1;
 		}
 
-		this.scene.pushMatrix();
+		let transformMatrix = mat4.create();
+		mat4.identity(transformMatrix);
+		mat4.translate(transformMatrix, transformMatrix, this.center);
+		mat4.rotate(transformMatrix, transformMatrix, this.startAng + (this.rotAng * t), [0, 1, 0]);
+		mat4.translate(transformMatrix, transformMatrix, [this.radius, 0, 0]);
+		mat4.rotate(transformMatrix, transformMatrix, -Math.PI/2, [0, 1, 0]);
+
+		return transformMatrix;
+		
+
+		/* this.scene.pushMatrix();
 			this.scene.loadIdentity();
 			this.scene.translate(this.centerX, this.centerY, this.centerZ);
 			this.scene.rotate(-(this.startAng + (this.rotAng * t)), 0, 1, 0);
 			this.scene.translate(this.radius, 0, 0);
 			this.scene.rotate(-Math.PI/2, 0, 1, 0);
-			this.transformMatrix = this.scene.getMatrix();
+			let transformMatrix = this.scene.getMatrix();
 		this.scene.popMatrix();
 
-		return this.transformMatrix;
+		return transformMatrix; */
 	}
 
 	_getTotalDistance() {
-		return this.rotAng * this.radius;
+		return Math.abs(this.rotAng * this.radius);
 	}
 }
